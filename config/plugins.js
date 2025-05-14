23ff1d72666c52a634fad03f8bef3fc8a2fe6243
Ginori Carrascal <ginorigigey@gmail.com>
2025-05-13 23:58:46 -0500

    Actualiza conexión a base de datos para Heroku

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,6 @@
-
 module.exports = {
-    
-  };
-  
+  // Otros plugins...
+  'users-permissions': {
+    jwtSecret: process.env.jwtSecret || 'your-default-jwtSecret', // Opcional: agrega un valor predeterminado para desarrollo
+  },
+};
